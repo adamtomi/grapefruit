f391aed3639ec1d5cb7c1dc07e68c530fbb51a51
HgeX <atomi2500@gmail.com>
2021-10-09 16:03:33 +0300

    progress

--- src/main/java/grapefruit/command/dispatcher/SuggestionHelper.java
+++ src/main/java/grapefruit/command/dispatcher/SuggestionHelper.java
@@ -1,56 +1,110 @@
 package grapefruit.command.dispatcher;
 
+import grapefruit.command.CommandException;
 import grapefruit.command.dispatcher.registration.CommandRegistration;
 import grapefruit.command.parameter.CommandParameter;
 import grapefruit.command.parameter.FlagParameter;
 import grapefruit.command.util.Miscellaneous;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Queue;
+import java.util.regex.Matcher;
 
-import static java.util.Objects.requireNonNull;
+// string(asd) string(test0) string(test1) boolean-flag(b) value-flag(d) value-flag(c)
+// input: asd -bcd 100 $
+// \_ sugg(d)
 
+// input: asd test0 -b test1 -c 45 $
+// \_ sugg(d)
+
+// input: asd -$
+// \_ test0 is string -> sugg(-b, -c, -d) # if test0 is number -> sugg(numbers)
+
+// input: asd -b$
+// \_ sugg(-bc, -bd)
+
+// input: asd -cd$
+// \_ sugg(-cdb)
+
+// input: asd -cd $
+// \_ sugg(c) cache(sugg(d))
 class SuggestionHelper<S> {
-    private final CommandAuthorizer<S> authorizer;
+    static final String SUGGEST_ME = "__SUGGEST_ME__";
 
-    SuggestionHelper(final @NotNull CommandAuthorizer<S> authorizer) {
-        this.authorizer = requireNonNull(authorizer, "authorizer cannot be null");
-    }
+    SuggestionHelper() {}
 
     public @NotNull List<String> listSuggestions(final @NotNull CommandContext<S> commandContext,
-                                                 final @NotNull String prevArg,
                                                  final @NotNull CommandRegistration<S> registration,
-                                                 final @NotNull Queue<CommandInput> args) {
-        final List<CommandParameter<S>> parameters = registration.parameters();
-        final String currentArg = args.remove().rawArg();
-
-        if (prevArg.startsWith("--")) {
-            // TODO cleanup
-            final Optional<FlagParameter<S>> flag = parameters.stream()
-                    .filter(CommandParameter::isFlag)
-                    .map(param -> (FlagParameter<S>) param)
-                    .filter(param -> param.flagName().equalsIgnoreCase(prevArg.substring(2)))
-                    .filter(param -> commandContext.find(param.flagName()).isEmpty())
-                    .findFirst();
-            return flag.map(param -> param.mapper().listSuggestions(commandContext, currentArg, param.modifiers()))
-                    .orElse(List.of());
+                                                 final @NotNull Queue<CommandInput> args) throws CommandException {
+        System.out.println("SuggestionHelper#listSuggestions");
+        final Optional<CommandParameter<S>> parameterOpt = commandContext.find(SUGGEST_ME);
+        System.out.println(parameterOpt);
+        if (parameterOpt.isEmpty()) {
+            return List.of();
         }
 
-        final Optional<CommandParameter<S>> firstNonFlagParameter = parameters.stream()
-                .filter(param -> !param.isFlag())
-                .filter(param -> commandContext.find(param.name()).isEmpty())
-                .findFirst();
-        final List<CommandParameter<S>> flags = parameters.stream()
-                .filter(CommandParameter::isFlag)
-                .filter(param -> commandContext.find(Miscellaneous.parameterName(param)).isEmpty())
-                .toList();
-        firstNonFlagParameter.ifPresent(flags::add);
-        return flags.stream()
-                .map(param -> param.mapper().listSuggestions(commandContext, currentArg, param.modifiers()))
-                .flatMap(Collection::stream)
-                .toList();
+        final CommandParameter<S> parameter = parameterOpt.orElseThrow();
+        final String currentArg = args.element().rawArg();
+        final List<String> suggestions = parameter.mapper().listSuggestions(commandContext, currentArg, parameter.modifiers());
+        final Matcher matcher = FlagParameter.FLAG_PATTERN.matcher(currentArg);
+
+        if (matcher.matches()) {
+            final List<String> flagSuggestions = new ArrayList<>();
+            final List<FlagParameter<S>> allFlags = registration.parameters()
+                    .stream()
+                    .filter(CommandParameter::isFlag)
+                    .map(x -> (FlagParameter<S>) x)
+                    .toList();
+            for (final FlagParameter<S> each : allFlags) {
+                if (commandContext.find(each.flagName()).isPresent()) {
+                    continue;
+                }
+
+                flagSuggestions.add(Miscellaneous.formatFlag(each.flagName()));
+                flagSuggestions.add(Miscellaneous.formatFlag(String.valueOf(each.shorthand())));
+            }
+            /*for (final CommandParameter<S> each : allFlags) {
+                if (!each.isFlag()) {
+                    continue;
+                }
+
+                final boolean shorthandOnly = matcher.group(1).isEmpty();
+                final String nameFragment = matcher.group(2);
+                final boolean suggestAllFlags = nameFragment.isEmpty();
+                final List<FlagParameter<S>> possibleFlags = allFlags.stream()
+                        .filter(CommandParameter::isFlag)
+                        .map(x -> (FlagParameter<S>) x)
+                        .filter(x -> commandContext.find(x.flagName()).isEmpty())
+                        .toList();
+
+                if (shorthandOnly) {
+                    if (suggestAllFlags) {
+                        possibleFlags.forEach(x -> {
+                            flagSuggestions.add(x.flagName());
+                            final char shorthand = x.shorthand();
+                            if (shorthandNotEmpty(shorthand)) {
+                                flagSuggestions.add(String.valueOf(shorthand));
+                            }
+                        });
+                    } else {
+                        possibleFlags.forEach(x -> {
+                            final char shorthand = x.shorthand();
+                            if (shorthandNotEmpty(shorthand) && ) {
+
+                            }
+                        });
+                    }
+                }
+                // shorthandonly -> flag names, flag shorthands, flag groups
+                // !shorthandonly -> flag names
+                suggestions.addAll(flagSuggestions);
+            }*/
+            suggestions.addAll(flagSuggestions);
+        }
+
+        return suggestions;
     }
 }
