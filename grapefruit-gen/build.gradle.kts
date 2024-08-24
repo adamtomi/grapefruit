a4c87a5f3e1686296c926f805bcbbdaccd6f4b66
adamtomi <atomi2500@gmail.com>
2024-08-24 16:50:12 +0200

    add gen module

---
+++ grapefruit-gen/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "grapefruit"
+version = "2.0.0-ALPHA"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
