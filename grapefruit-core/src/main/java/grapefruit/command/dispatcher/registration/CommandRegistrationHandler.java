409f6c8ad6bd2e17500364f8d5bc0fe9bc27dee9
adamtomi <atomi2500@gmail.com>
2024-08-24 16:52:56 +0200

    delete old files

--- grapefruit-core/src/main/java/grapefruit/command/dispatcher/registration/CommandRegistrationHandler.java
+++
@@ -1,9 +0,0 @@
-package grapefruit.command.dispatcher.registration;
-
-import java.util.function.Consumer;
-
-@FunctionalInterface
-public interface CommandRegistrationHandler<S> extends Consumer<CommandRegistrationContext<S>> {
-
-    CommandRegistrationHandler<?> NO_OP = context -> {};
-}
