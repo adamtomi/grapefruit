a65cc71ea1f10db48bcef4c0f18c432cdda5260a
adamtomi <atomi2500@gmail.com>
2024-08-25 23:18:48 +0200

    add missing serialVersionUID

--- grapefruit-core/src/main/java/grapefruit/command/dispatcher/CommandException.java
+++ grapefruit-core/src/main/java/grapefruit/command/dispatcher/CommandException.java
@@ -1,3 +1,12 @@
 package grapefruit.command.dispatcher;
 
-public class CommandException extends Exception {}
+import java.io.Serial;
+
+public class CommandException extends Exception {
+    @Serial
+    private static final long serialVersionUID = -1522184165136400665L;
+
+    public CommandException() {
+        super();
+    }
+}
