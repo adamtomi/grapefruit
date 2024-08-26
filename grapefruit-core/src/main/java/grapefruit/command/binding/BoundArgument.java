ffd8c42e7a4717b7373ed8b993b0fa9f08442870
adamtomi <atomi2500@gmail.com>
2024-08-26 08:40:32 +0200

    delete BoundArgument

--- grapefruit-core/src/main/java/grapefruit/command/binding/BoundArgument.java
+++
@@ -1,9 +0,0 @@
-package grapefruit.command.binding;
-
-import grapefruit.command.dispatcher.CommandContext;
-import grapefruit.command.dispatcher.CommandException;
-
-public interface BoundArgument<T> {
-
-    T get(CommandContext context) throws CommandException;
-}
