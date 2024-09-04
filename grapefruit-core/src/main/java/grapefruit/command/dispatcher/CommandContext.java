468255c6b7292f1ce49d62519686949ae710b539
adamtomi <atomi2500@gmail.com>
2024-09-04 15:10:57 +0200

    commandcontext improvements

--- grapefruit-core/src/main/java/grapefruit/command/dispatcher/CommandContext.java
+++ grapefruit-core/src/main/java/grapefruit/command/dispatcher/CommandContext.java
@@ -2,21 +2,31 @@ package grapefruit.command.dispatcher;
 
 import grapefruit.command.util.Registry;
 import grapefruit.command.util.key.Key;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 
-// TODO refactor command context
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Stores data related to the current command execution. This data
+ * mainly consists of parsed values of command arguments, but
+ * any value can be stored with a {@link Key key} instance. Command
+ * handler functions are able to retrieve these values.
+ */
 public class CommandContext {
     private final Registry<Key<?>, Object> internalStore = Registry.create();
 
     // TODO
     @SuppressWarnings("unchecked")
+    @Deprecated(forRemoval = true)
     public <T> Optional<T> getSafe(Key<T> key) {
         return (Optional<T>) this.internalStore.get(key) ;
     }
 
     // TODO
-    public <T> T get(Key<T> key) {
+    @Deprecated(forRemoval = true)
+    public <T> T _get(Key<T> key) {
         return getSafe(key).orElseThrow();
     }
 
@@ -24,8 +34,57 @@ public class CommandContext {
         return this.internalStore.has(key);
     }
 
+    @Deprecated(forRemoval = true)
     public <T> void store(Key<T> key, T instance) {
         if (this.internalStore.has(key)) throw new IllegalStateException("Key '%s' is already stored in this context".formatted(key));
         this.internalStore.store(key, instance);
     }
+
+    /**
+     * Finds and returns the value associated with the
+     * provided key, wrapped in an {@link Optional}. If
+     * no value is mapped to the key, {@link Optional#empty()}
+     * is returned.
+     *
+     * @param <T> The expected argument type
+     * @param key The key
+     * @return The found value, or an empty optional
+     */
+    public <T> Optional<T> get(Key<T> key) {
+        requireNonNull(key, "key cannot be null");
+        return Optional.ofNullable(nullable(key));
+    }
+
+    /**
+     * Finds and returns the value associated with the
+     * provided key, or throws an {@link IllegalArgumentException},
+     * if nothing is mapped to the key.
+     *
+     * @param <T> The expected argument type
+     * @param key The key
+     * @return The found value
+     * @throws IllegalArgumentException If nothing is mapped to the key
+     */
+    public <T> T need(Key<T> key) {
+        requireNonNull(key, "key cannot be null");
+        // TODO probably need to throw a different exception. Maybe
+        return get(key).orElseThrow(() -> new IllegalArgumentException("Nothing is mapped to key '%s'".formatted(key)));
+    }
+
+    /**
+     * Find and returns the value associated with the
+     * provided key, or null if nothing is mapped to
+     * the key.
+     *
+     * @param <T> The expected argument type
+     * @param key The key
+     * @return The value, or null
+     */
+    @SuppressWarnings("unchecked")
+    public <T> @Nullable T nullable(Key<T> key) {
+        requireNonNull(key, "key cannot be null");
+        return (T) this.internalStore.get(key);
+    }
+
+    // TODO add remove, contains, asMap() and such
 }
