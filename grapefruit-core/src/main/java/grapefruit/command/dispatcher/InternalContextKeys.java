f482744c9280b0fdf79abdeff215817f99d7ef5f
adamtomi <atomi2500@gmail.com>
2024-09-06 10:58:55 +0300

    add documentation

--- grapefruit-core/src/main/java/grapefruit/command/dispatcher/InternalContextKeys.java
+++ grapefruit-core/src/main/java/grapefruit/command/dispatcher/InternalContextKeys.java
@@ -3,6 +3,10 @@ package grapefruit.command.dispatcher;
 import grapefruit.command.Command;
 import grapefruit.command.util.key.Key;
 
+/**
+ * This class stores keys that are required by
+ * grapefruit internals to work properly.
+ */
 public final class InternalContextKeys {
     public static final Key<Command> COMMAND = Key.named(Command.class, "grapefruit:internal:command");
 
