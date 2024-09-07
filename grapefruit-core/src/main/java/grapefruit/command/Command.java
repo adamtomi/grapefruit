f13ca05eefbd5969afc0c36feb45a267782e0867
adamtomi <atomi2500@gmail.com>
2024-09-07 11:21:02 +0300

    add more documentation

--- grapefruit-core/src/main/java/grapefruit/command/Command.java
+++ grapefruit-core/src/main/java/grapefruit/command/Command.java
@@ -7,12 +7,48 @@ import grapefruit.command.dispatcher.CommandSpec;
 
 import java.util.List;
 
+/**
+ * Represents an executable command. A command has a couple of
+ * attributes:
+ * <ul>
+ *     <li>A list of {@link CommandArgument command arguments}</li>
+ *     <li>A route at which the command will be available</li>
+ *     <li>A permission that is required to execute the command <bold>(Optional)</bold></li>
+ *     <li>A list of conditions that first need to pass before executing the actual command <bold>(Optional)</bold></li>
+ * </ul>
+ *
+ * The code generator module will take command methods annotated with {@link grapefruit.command.annotation.CommandDefinition}
+ * and through the use of {@link Command#wrap(List, CommandSpec, CommandExecutable)} will generate
+ * executable command instances. These commands can be invoked without relying on the use of
+ * reflection, increasing performance.
+ */
 public interface Command extends CommandExecutable {
 
+    /**
+     * Returns an immutable list view of the arguments
+     * of this command.
+     *
+     * @return List of arguments
+     */
     List<CommandArgument<?>> arguments();
 
+    /**
+     * Returns the command spec of this command.
+     * @see CommandSpec
+     *
+     * @return The spec
+     */
     CommandSpec spec();
 
+    /**
+     * Creates a new command instance wrapping the provided arguments, spec and action. This method
+     * is used by the code generator module and is considered an internal API.
+     *
+     * @param arguments The arguments
+     * @param spec The command spec
+     * @param action The action to execute
+     * @return The created command
+     */
     static Command wrap(List<CommandArgument<?>> arguments, CommandSpec spec, CommandExecutable action) {
         return new Command() {
             @Override
--- grapefruit-core/src/main/java/grapefruit/command/CommandExecutable.java
+++ grapefruit-core/src/main/java/grapefruit/command/CommandExecutable.java
@@ -2,8 +2,21 @@ package grapefruit.command;
 
 import grapefruit.command.dispatcher.CommandContext;
 
+/**
+ * An executable part of a command.
+ * @see Command#execute(CommandContext)
+ * @see grapefruit.command.argument.chain.BoundArgument#execute(CommandContext)
+ */
 @FunctionalInterface
 public interface CommandExecutable {
 
+    /**
+     * Executes this part of the comand chain using the
+     * provided context.
+     *
+     * @param context The current context
+     * @throws CommandException If anything goes wrong during
+     * the execution
+     */
     void execute(CommandContext context) throws CommandException;
 }
