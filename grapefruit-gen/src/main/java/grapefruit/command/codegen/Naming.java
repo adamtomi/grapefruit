332e265ae06c3af5165646d58cc3968ccb5c766d
Tamás Ádám <atomi2500@gmail.com>
2024-09-09 18:45:39 +0300

    remove unused names

--- grapefruit-gen/src/main/java/grapefruit/command/codegen/Naming.java
+++ grapefruit-gen/src/main/java/grapefruit/command/codegen/Naming.java
@@ -10,9 +10,6 @@ public final class Naming {
     public static final String COMMANDS = "commands";
     public static final Function<TypeElement, String> CONTAINER_CLASS_SUFFIX = clazz -> "%s_Container".formatted(clazz.getSimpleName());
     public static final String CONTEXT_PARAM = "context";
-    public static final String FACTORY_SUFFIX = "_Factory";
-    public static final String GENERATE_METHOD = "generate";
-    public static final String INTERNAL_FACTORY_FIELD = "internalFactory";
     public static final String KEY_FIELD_SUFFIX = "_key";
     public static final String REFERENCE_PARAM = "reference";
     public static final String RESULT = "result";
