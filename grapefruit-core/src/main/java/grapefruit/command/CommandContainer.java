6278b6f006a23d0a4d44bebebc52ac9135601f41
Tamás Ádám <atomi2500@gmail.com>
2024-09-11 12:48:20 +0300

    Add javadoc, delete CommandFactory

--- grapefruit-core/src/main/java/grapefruit/command/CommandContainer.java
+++ grapefruit-core/src/main/java/grapefruit/command/CommandContainer.java
@@ -2,7 +2,22 @@ package grapefruit.command;
 
 import java.util.Set;
 
+/**
+ * Every java class containing at least one method annotated with
+ * {@link grapefruit.command.annotation.CommandDefinition} a
+ * container class implementing this interface will be generated.
+ * This makes it easier to retrieve and use the generated {@link Command}
+ * instances.
+ */
 public interface CommandContainer {
 
+    /**
+     * Creates and returns an immutable {@link Set} of generated
+     * {@link Command} instances, that correspond to methods
+     * found in the original container class this container is generated
+     * for.
+     *
+     * @return The generated commands
+     */
     Set<Command> commands();
 }
