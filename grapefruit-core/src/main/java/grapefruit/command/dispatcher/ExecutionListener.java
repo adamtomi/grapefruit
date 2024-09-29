90b321e572db9e986ae325ca43cf229132ff3ed7
adamtomi <atomi2500@gmail.com>
2024-09-29 11:26:46 +0300

    add more documentation

--- grapefruit-core/src/main/java/grapefruit/command/dispatcher/ExecutionListener.java
+++ grapefruit-core/src/main/java/grapefruit/command/dispatcher/ExecutionListener.java
@@ -1,7 +1,21 @@
 package grapefruit.command.dispatcher;
 
+/**
+ * Execution listeners are invoked by the {@link CommandDispatcher}
+ * at various stages of command execution. The exact {@link ExecutionStage}
+ * is set when registering the listener in {@link grapefruit.command.dispatcher.config.DispatcherConfigurer}.
+ */
 @FunctionalInterface
 public interface ExecutionListener {
 
+    /**
+     * An execution listener may decide to interrupt command
+     * execution, by returning false in this method. Called
+     * by the {@link CommandDispatcher} at the appropriate
+     * stage.
+     *
+     * @param context The current context
+     * @return Whether to proceed with command execution
+     */
     boolean handle(CommandContext context);
 }
--- grapefruit-core/src/main/java/grapefruit/command/dispatcher/ExecutionStage.java
+++ grapefruit-core/src/main/java/grapefruit/command/dispatcher/ExecutionStage.java
@@ -1,7 +1,24 @@
 package grapefruit.command.dispatcher;
 
+/**
+ * Execution stage is used to by {@link ExecutionListener listeners}.
+ */
 public enum ExecutionStage {
+    /**
+     * At this stage, the command to execute has been found, but
+     * user ipnut has not been parsed into command arguments yet.
+     */
     PRE_PROCESS,
+    /**
+     * At this tage, the command instance has been found and user
+     * input has been parsed into command arguments, but the command
+     * has not been invoked yet.
+     */
     PRE_EXECUTION,
+    /**
+     * At this stage, the command has been found and executed,
+     * and the result of this execution (if any) is available
+     * in the current {@link CommandContext context}.
+     */
     POST_EXECUTION
 }
