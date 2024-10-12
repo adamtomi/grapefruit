18c683015a905b4140499b2074bffdd8ef470042
Tamás Ádám <atomi2500@gmail.com>
2024-10-12 16:28:30 +0300

    test stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,20 @@
+plugins {
+    id("java")
+    id("jacoco")
+    id("grapefruit.java-conventions")
+}
+
 group = "grapefruit"
 version = "2.0.0-ALPHA"
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+}
+
+dependencies {
+    testRuntimeOnly(libs.jupiter.engine)
+}
--- grapefruit-runtime/build.gradle.kts
+++ grapefruit-runtime/build.gradle.kts
@@ -1,81 +1,13 @@
 plugins {
     id("java")
-    id("maven-publish")
-    id("jacoco")
     id("grapefruit.java-conventions")
 }
 
 dependencies {
     compileOnly(libs.annotations)
     implementation(libs.geantyref)
+
+    testImplementation(libs.jupiter.api)
+    testRuntimeOnly(libs.jupiter.engine)
+    testImplementation(libs.jupiter.params)
 }
-
-
-
-/*
-
-
-test {
-    useJUnitPlatform()
-}
-
-test.finalizedBy jacocoTestReport
-jacocoTestReport.dependsOn test
-
-task sourcesJar(type: Jar) {
-    archiveClassifier = 'sources'
-    from sourceSets.main.java.srcDirs
-}
-
-artifacts {
-    archives sourcesJar
-}
-
-publishing {
-    publications {
-        mavenJava(MavenPublication) {
-            artifactId = 'grapefruit'
-            from components.java
-            artifact sourcesJar
-
-            versionMapping {
-                usage('java-api') {
-                    fromResolutionOf('runtimeClasspath')
-                }
-                usage('java-runtime') {
-                    fromResolutionResult()
-                }
-            }
-            pom {
-                name = 'Grapefruit'
-                description = 'A command parsing library'
-                url = 'https://github.com/HgeX/grapefruit'
-                licenses {
-                    license {
-                        name = 'GNU General Public License v3.0'
-                        url = 'https://www.gnu.org/licenses/gpl-3.0.txt'
-                    }
-                }
-            }
-
-            pom.withXml {
-                asNode().dependencies.'*'.findAll() {
-                    it.scope.text() == 'runtime'  && project.configurations.default.allDependencies.find { dep ->
-                        dep.name == it.artifactId.text()
-                    }
-                }.each() {
-                    it.scope*.value = 'compile'
-                }
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            def releasesRepoUrl = layout.buildDirectory.dir('repos/releases')
-            def snapshotsRepoUrl = layout.buildDirectory.dir('repos/snapshots')
-            url = version.endsWith('SNAPSHOT') ? snapshotsRepoUrl : releasesRepoUrl
-        }
-    }
-}
-*/
