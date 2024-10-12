c745d444b55865f471db3ab089c1b14c732f5e33
Tamás Ádám <atomi2500@gmail.com>
2024-10-12 13:33:20 +0300

    kotlin

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "grapefruit"
+include("grapefruit-compiler")
+include("grapefruit-runtime")
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
