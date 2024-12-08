b52afb255d10514cef3899f89bcbe6bbf0709519
Tamás Ádám <41118733+adamtomi@users.noreply.github.com>
2024-12-08 14:42:46 +0200

    add `CommandGraph` tests

--- src/test/java/grapefruit/command/tree/CommandGraphTests.java
+++ src/test/java/grapefruit/command/tree/CommandGraphTests.java
@@ -1,4 +1,200 @@
 package grapefruit.command.tree;
 
+import grapefruit.command.CommandModule;
+import grapefruit.command.argument.CommandChain;
+import grapefruit.command.argument.CommandChainFactory;
+import grapefruit.command.dispatcher.input.CommandInputTokenizer;
+import grapefruit.command.dispatcher.input.CommandSyntaxException;
+import grapefruit.command.util.EmptyCommandChain;
+import grapefruit.command.util.TestCommandModule;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class CommandGraphTests {
+
+    @Test
+    public void insert_emptyTree() {
+        final CommandGraph<Object> graph = new CommandGraph<>();
+        final CommandChain<Object> chain = new EmptyCommandChain();
+
+        assertThrows(IllegalStateException.class, () -> graph.insert(chain, TestCommandModule.dummy()));
+    }
+
+    @Test
+    public void insert_ambiguousTree() {
+        final CommandGraph<Object> graph = new CommandGraph<>();
+        final CommandModule<Object> module = TestCommandModule.of(factory -> factory.newChain()
+                .then(factory.literal("test").build()).build());
+
+        final CommandChainFactory<Object> factory = CommandChain.factory();
+        final CommandChain<Object> chain = module.chain(factory);
+
+        assertDoesNotThrow(() -> graph.insert(chain, module));
+        assertThrows(IllegalStateException.class, () -> graph.insert(chain, TestCommandModule.dummy()));
+    }
+
+    @Test
+    public void insert_success() {
+        final CommandGraph<Object> graph = new CommandGraph<>();
+        final CommandModule<Object> module = TestCommandModule.of(factory -> factory.newChain()
+                .then(factory.literal("test").build()).build());
+
+        final CommandChainFactory<Object> factory = CommandChain.factory();
+        final CommandChain<Object> chain = module.chain(factory);
+
+        graph.insert(chain, module);
+        assertDoesNotThrow(() -> assertEquals(module, graph.search(CommandInputTokenizer.wrap("test"))));
+    }
+
+    @Test
+    public void insert_treeIntegrity() {
+        final CommandGraph<Object> graph = new CommandGraph<>();
+        final CommandChainFactory<Object> factory = CommandChain.factory();
+
+        final CommandChain<Object> chain1 = factory.newChain()
+                .then(factory.literal("test").build())
+                .then(factory.literal("other").build()).build();
+
+        final CommandChain<Object> chain2 = factory.newChain()
+                .then(factory.literal("test").build())
+                .then(factory.literal("next").build()).build();
+
+        final CommandChain<Object> chain3 = factory.newChain()
+                .then(factory.literal("command").build()).build();
+
+        final CommandModule<Object> command1 = TestCommandModule.computed(chain1);
+        final CommandModule<Object> command2 = TestCommandModule.computed(chain2);
+        final CommandModule<Object> command3 = TestCommandModule.computed(chain3);
+
+        graph.insert(chain1, command1);
+        graph.insert(chain2, command2);
+        graph.insert(chain3, command3);
+
+        assertDoesNotThrow(() -> assertEquals(command1, graph.search(CommandInputTokenizer.wrap("test other"))));
+        assertDoesNotThrow(() -> assertEquals(command2, graph.search(CommandInputTokenizer.wrap("test next"))));
+        assertDoesNotThrow(() -> assertEquals(command3, graph.search(CommandInputTokenizer.wrap("command"))));
+    }
+
+    @Test
+    public void delete_noSuchChild_emptyTree() {
+        final CommandGraph<Object> graph = new CommandGraph<>();
+        final CommandChainFactory<Object> factory = CommandChain.factory();
+        final CommandChain<Object> chain = factory.newChain()
+                .then(factory.literal("test").build()).build();
+
+        assertThrows(IllegalStateException.class, () -> graph.delete(chain));
+    }
+
+    @Test
+    public void delete_noSuchChild() {
+        final CommandGraph<Object> graph = new CommandGraph<>();
+        final CommandChainFactory<Object> factory = CommandChain.factory();
+
+        final CommandChain<Object> chain1 = factory.newChain()
+                .then(factory.literal("test").build()).build();
+        final CommandChain<Object> chain2 = factory.newChain()
+                        .then(factory.literal("other").build()).build();
+
+        graph.insert(chain1, TestCommandModule.dummy());
+        assertThrows(IllegalStateException.class, () -> graph.delete(chain2));
+    }
+
+    @Test
+    public void delete_nonLeafNode() {
+        final CommandGraph<Object> graph = new CommandGraph<>();
+        final CommandChainFactory<Object> factory = CommandChain.factory();
+
+        final CommandChain<Object> chain1 = factory.newChain()
+                .then(factory.literal("test").build())
+                .then(factory.literal("next").build())
+                .build();
+
+        final CommandChain<Object> chain2 = factory.newChain()
+                .then(factory.literal("test").build()).build();
+
+        graph.insert(chain1, TestCommandModule.dummy());
+        assertThrows(IllegalStateException.class, () -> graph.delete(chain2));
+    }
+
+    @Test
+    public void delete_success() {
+        final CommandGraph<Object> graph = new CommandGraph<>();
+        final CommandChainFactory<Object> factory = CommandChain.factory();
+        final CommandChain<Object> chain = factory.newChain()
+                .then(factory.literal("test").build()).build();
+
+        final CommandModule<Object> command = TestCommandModule.computed(chain);
+        graph.insert(chain, command);
+        graph.delete(chain);
+
+        assertThrows(NoSuchCommandException.class, () -> graph.search(CommandInputTokenizer.wrap("test")));
+    }
+
+    @Test
+    public void delete_treeIntegrity() {
+        final CommandGraph<Object> graph = new CommandGraph<>();
+        final CommandChainFactory<Object> factory = CommandChain.factory();
+
+        final CommandChain<Object> chain1 = factory.newChain()
+                .then(factory.literal("test").build())
+                .then(factory.literal("other").build()).build();
+
+        final CommandChain<Object> chain2 = factory.newChain()
+                .then(factory.literal("test").build())
+                .then(factory.literal("next").build()).build();
+
+        final CommandChain<Object> chain3 = factory.newChain()
+                .then(factory.literal("command").build()).build();
+
+        final CommandModule<Object> command1 = TestCommandModule.computed(chain1);
+        final CommandModule<Object> command2 = TestCommandModule.computed(chain2);
+        final CommandModule<Object> command3 = TestCommandModule.computed(chain3);
+
+        graph.insert(chain1, command1);
+        graph.insert(chain2, command2);
+        graph.insert(chain3, command3);
+
+        graph.delete(chain1);
+        assertThrows(NoSuchCommandException.class, () -> graph.search(CommandInputTokenizer.wrap("test other")));
+        assertDoesNotThrow(() -> assertEquals(command2, graph.search(CommandInputTokenizer.wrap("test next"))));
+        assertDoesNotThrow(() -> assertEquals(command3, graph.search(CommandInputTokenizer.wrap("command"))));
+    }
+
+    @Test
+    public void search_emptyTree() {
+        final CommandGraph<Object> graph = new CommandGraph<>();
+        assertThrows(NoSuchCommandException.class, () -> graph.search(CommandInputTokenizer.wrap("test")));
+    }
+
+    @Test
+    public void search_emptyTree_emptyInput() {
+        final CommandGraph<Object> graph = new CommandGraph<>();
+        assertThrows(CommandSyntaxException.class, () -> graph.search(CommandInputTokenizer.wrap("")));
+    }
+
+    @Test
+    public void search_noSuchCommand() {
+        final CommandGraph<Object> graph = new CommandGraph<>();
+        final CommandChainFactory<Object> factory = CommandChain.factory();
+        final CommandChain<Object> chain = factory.newChain()
+                .then(factory.literal("test").build()).build();
+
+        graph.insert(chain, TestCommandModule.dummy());
+        assertThrows(NoSuchCommandException.class, () -> graph.search(CommandInputTokenizer.wrap("hello")));
+    }
+
+    @Test
+    public void search_success() {
+        final CommandGraph<Object> graph = new CommandGraph<>();
+        final CommandChainFactory<Object> factory = CommandChain.factory();
+        final CommandChain<Object> chain = factory.newChain()
+                .then(factory.literal("test").build()).build();
+        final CommandModule<Object> command = TestCommandModule.computed(chain);
+
+        graph.insert(chain, command);
+        assertDoesNotThrow(() -> assertEquals(command, graph.search(CommandInputTokenizer.wrap("test"))));
+    }
 }
--- src/test/java/grapefruit/command/util/TestCommandModule.java
+++ src/test/java/grapefruit/command/util/TestCommandModule.java
@@ -10,8 +10,20 @@ import java.util.function.Function;
 public class TestCommandModule implements CommandModule<Object> {
     private final Function<CommandChainFactory<Object>, CommandChain<Object>> chainFactory;
 
-    public TestCommandModule(final Function<CommandChainFactory<Object>, CommandChain<Object>> chainFactory) {
+    private TestCommandModule(final Function<CommandChainFactory<Object>, CommandChain<Object>> chainFactory) {
         this.chainFactory = chainFactory;
+    }
+
+    public static TestCommandModule of(final Function<CommandChainFactory<Object>, CommandChain<Object>> chainFactory) {
+        return new TestCommandModule(chainFactory);
+    }
+
+    public static TestCommandModule computed(final CommandChain<Object> chain) {
+        return new TestCommandModule(x -> chain);
+    }
+
+    public static TestCommandModule dummy() {
+        return new TestCommandModule(factory -> new EmptyCommandChain());
     }
 
     @Override
