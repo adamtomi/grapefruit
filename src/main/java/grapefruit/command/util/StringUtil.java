1fbe59659818d1597a27504b696793b6b3a0f345
Tamás Ádám <41118733+adamtomi@users.noreply.github.com>
2024-12-10 17:18:25 +0200

    add missing final

--- src/main/java/grapefruit/command/util/StringUtil.java
+++ src/main/java/grapefruit/command/util/StringUtil.java
@@ -4,9 +4,7 @@ import java.util.Collection;
 import java.util.function.Function;
 
 public final class StringUtil {
-    private StringUtil() {
-        throw new DontInvokeMe();
-    }
+    private StringUtil() {}
 
     public static <T extends Collection<String>> boolean containsIgnoreCase(final String item, final T items) {
         return items.stream().anyMatch(item::equalsIgnoreCase);
@@ -23,7 +21,7 @@ public final class StringUtil {
     }
 
     public static boolean containsWhitespace(final String arg) {
-        for (char c : arg.toCharArray()) {
+        for (final char c : arg.toCharArray()) {
             if (Character.isWhitespace(c)) return true;
         }
 
