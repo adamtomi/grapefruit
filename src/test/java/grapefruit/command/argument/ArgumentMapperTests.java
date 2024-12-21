b094111fb49684cd87af5147c8b43561a3cb6231
adamtomi <atomi2500@gmail.com>
2024-12-21 17:18:14 +0100

    add argument mapper tests

---
+++ src/test/java/grapefruit/command/argument/ArgumentMapperTests.java
@@ -0,0 +1,97 @@
+package grapefruit.command.argument;
+
+import grapefruit.command.argument.mapper.ArgumentMappingException;
+import grapefruit.command.argument.mapper.builtin.EnumArgumentMapper;
+import grapefruit.command.mock.NilCommandContext;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
+
+import static grapefruit.command.argument.mapper.builtin.NumericArgumentMapper.floatMapper;
+import static grapefruit.command.argument.mapper.builtin.NumericArgumentMapper.intMapper;
+import static grapefruit.command.argument.mapper.builtin.StringArgumentMapper.greedy;
+import static grapefruit.command.argument.mapper.builtin.StringArgumentMapper.quotable;
+import static grapefruit.command.argument.mapper.builtin.StringArgumentMapper.regex;
+import static grapefruit.command.argument.mapper.builtin.StringArgumentMapper.word;
+import static grapefruit.command.testutil.Helper.access;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ArgumentMapperTests {
+
+    @Test
+    public void stringArgumentMapper_word() {
+        final String word = "hello";
+        assertDoesNotThrow(() -> assertEquals(word, word().tryMap(new NilCommandContext(), access(word))));
+    }
+
+    @Test
+    public void stringArgumentMapper_quotable() {
+        final String content = "hello ";
+        final String command = "\"%s\" world".formatted(content);
+        assertDoesNotThrow(() -> assertEquals(content, quotable().tryMap(new NilCommandContext(), access(command))));
+    }
+
+    @Test
+    public void stringArgumentMapper_greedy() {
+        final String command = "hello world";
+        assertDoesNotThrow(() -> assertEquals(command, greedy().tryMap(new NilCommandContext(), access(command))));
+    }
+
+    @Test
+    public void stringArgumentMapper_regex_doesNotMatch() {
+        assertThrows(
+                ArgumentMappingException.class,
+                () -> word().with(regex(Pattern.compile("[a-z]+"))).tryMap(new NilCommandContext(), access("$hello$"))
+        );
+    }
+
+    @Test
+    public void stringArgumentMapper_regex_doesMatch() {
+        assertDoesNotThrow(
+                () -> word().with(regex(Pattern.compile("[a-z]+"))).tryMap(new NilCommandContext(), access("hello"))
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "''",
+            "abc",
+            "$",
+            "11.44",
+            "--344",
+            "2147483648" // 2^32 (Integer#MAX_VALUE + 1)
+    })
+    public void numericArgumentMapper_int_invalidInput(final String arg) {
+        assertThrows(ArgumentMappingException.class, () -> intMapper().tryMap(new NilCommandContext(), access(arg)));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "44",
+            "-44",
+            "3.14",
+            "-3.14"
+    })
+    public void numericArgumentMapper_float_validInput(final String arg) {
+        assertDoesNotThrow(() -> floatMapper().tryMap(new NilCommandContext(), access(arg)));
+    }
+
+    @Test
+    public void enumArgumentMapper_strict_tryMap() {
+        final EnumArgumentMapper<Object, TimeUnit> mapper = EnumArgumentMapper.strict(TimeUnit.class);
+        assertThrows(ArgumentMappingException.class, () -> mapper.tryMap(new NilCommandContext(), access("seconds")));
+        assertDoesNotThrow(() -> assertEquals(TimeUnit.SECONDS, mapper.tryMap(new NilCommandContext(), access("SECONDS"))));
+    }
+
+    @Test
+    public void enumArgumentMapper_lenient_tryMap() {
+        final EnumArgumentMapper<Object, TimeUnit> mapper = EnumArgumentMapper.lenient(TimeUnit.class);
+        assertDoesNotThrow(() -> assertEquals(TimeUnit.SECONDS, mapper.tryMap(new NilCommandContext(), access("seconds"))));
+        assertDoesNotThrow(() -> assertEquals(TimeUnit.SECONDS, mapper.tryMap(new NilCommandContext(), access("SECONDS"))));
+    }
+}
--- src/test/java/grapefruit/command/testutil/Helper.java
+++ src/test/java/grapefruit/command/testutil/Helper.java
@@ -1,5 +1,8 @@
 package grapefruit.command.testutil;
 
+import grapefruit.command.argument.mapper.CommandInputAccess;
+import grapefruit.command.dispatcher.input.CommandInputTokenizer;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,5 +12,9 @@ public final class Helper {
     public static List<String> toStringList(final String input) {
         if (input.isEmpty()) return List.of();
         return Arrays.asList(input.split("\\|"));
+    }
+
+    public static CommandInputAccess access(final String input) {
+        return CommandInputAccess.wrap(CommandInputTokenizer.wrap(input));
     }
 }
